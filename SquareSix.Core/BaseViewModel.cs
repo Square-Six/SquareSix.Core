8392abe740540b198adfbd08de06d1cfaf3268c6
Jamie Smith <jamie.smith@squaresix.com>
2020-10-06 21:25:25 -0700

    Analytics service, paging in base list view model

--- SquareSix.Core/BaseViewModel.cs
+++ SquareSix.Core/BaseViewModel.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace SquareSix.Core
 {
     public abstract class BaseViewModel : BaseCellModel
     {
         public bool IsBusy { get; set; }
-        public string Title { get; set; }
+
+        protected virtual string Title => string.Empty;
 
         public BaseViewModel()
         {
+        }
+
+        public virtual Task OnAppearing()
+        {
+            return Task.CompletedTask;
         }
     }
 }
