e4ade03f911613d08a7b4a4010cac89663116a43
Jamie Smith <jamie.smith@squaresix.com>
2020-12-11 16:10:59 -0800

    Removed any platform specific logic. Updated demo project

--- Demo/Demo.Forms/App.xaml.cs
+++ Demo/Demo.Forms/App.xaml.cs
@@ -11,13 +11,7 @@ namespace Demo.Forms
         {
             InitializeComponent();
 
-            var configuration = new SetupConfiguration
-            {
-                iOSAppCenterSecret = "<Your iOS AppCenter Secret>",
-                AndroidAppCenterSecret = "<Your Android AppCenter Secret>",
-                SecureCacheName = "<Your secure cache name>"
-            };
-            SquareSixService.Init(configuration);
+            SquareSixService.Init();
 
             MainPage = new AppShell();
         }
--- src/SquareSix.Core/SquareSixService.cs
+++ src/SquareSix.Core/SquareSixService.cs
@@ -9,25 +9,10 @@ namespace SquareSix.Core
 {
     public static class SquareSixService
     {
-        public static void Init(SetupConfiguration config)
+        public static void Init()
         {
-            if (config == null)
-            {
-                throw new ArgumentNullException("No 'SetupConfiguration' was found when running 'SquareSixService.Initialize'");
-            }
-
             // Register for alert service
             SimpleIOC.Container.Register<ISquaredAlertService>(new AlertService());
-
-            // Register for Analytics
-            var analyticsService = new SquaredAnalyticsService();
-            analyticsService.Setup(config?.iOSAppCenterSecret, config?.AndroidAppCenterSecret);
-            SimpleIOC.Container.Register<ISquaredAnalyticsService>(analyticsService);
-
-            // Register for Cacheing
-            var cacheService = new SquaredCacheService();
-            cacheService.Setup(config?.SecureCacheName);
-            SimpleIOC.Container.Register<ISquaredCacheService>(cacheService);
 
             // Register for Rest Services
             SimpleIOC.Container.Register<ISquaredRestService>(new SquaredRestService());
